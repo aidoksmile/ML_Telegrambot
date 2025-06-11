4389c451efa8d902e23662669d436bca70fe9d90
Aida Shakharova <109811676+aidoksmile@users.noreply.github.com>
2025-06-11 12:40:53 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-TELEGRAM_BOT_TOKEN = "TELEGRAM_BOT_TOKEN"  # Получите через @BotFather
-TELEGRAM_CHAT_ID = "TELEGRAM_CHAT_ID"      # Получите через getUpdates
+TELEGRAM_BOT_TOKEN = "8132037717:AAHK04i4cHkgqH-Qyiy7OHs73xbXmvqnlpc"  # Получите через @BotFather
+TELEGRAM_CHAT_ID = "106240757"      # Получите через getUpdates
 
-MT4_LOGIN = "MT4_LOGIN"                    # Логин вашего MT4-аккаунта
-MT4_PASSWORD = "MT4_PASSWORD"              # Пароль вашего MT4-аккаунта
-MT4_SERVER = "MT4_SERVER"                  # Сервер брокера (например, "MetaQuotes-Demo")
+MT4_LOGIN = "420493"                    # Логин вашего MT4-аккаунта
+MT4_PASSWORD = "E28yCis42FBZVY!"              # Пароль вашего MT4-аккаунта
+MT4_SERVER = "Capitol.com-Demo"                  # Сервер брокера (например, "MetaQuotes-Demo")
 
 ASSETS = ["XAUUSD", "EURUSD"]
 TICKER_MAP = {
