676947e6fdacdadf925b83c0c40fbf1a1a7cadda
Aida Shakharova <109811676+aidoksmile@users.noreply.github.com>
2025-06-14 13:33:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py
 import yfinance as yf
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
