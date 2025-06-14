c672128afedb9f571e74c446bae1fb45c46fb5d8
Aida Shakharova <109811676+aidoksmile@users.noreply.github.com>
2025-06-14 11:38:06 +0500

    Update and rename telegram_bot.py to send_telegram.py

---
+++ send_telegram.py
@@ -0,0 +1,18 @@
+# send_telegram.py
+import requests
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage" 
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+    try:
+        requests.post(url, json=payload)
+    except Exception as e:
+        print("Telegram error:", e)
