7eea6cd80686bf7413c1e94a4d4c6593b4e8e562
Aida Shakharova <109811676+aidoksmile@users.noreply.github.com>
2025-06-14 11:28:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask
 from predictor import generate_signals
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route('/')
 def home():
-    try:
-        signal = generate_signals()
-        return jsonify(signal)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    signal = generate_signals()
+    return f"Signal for EURUSD: {signal}"
